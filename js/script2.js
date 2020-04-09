9521ee76698e65bf3963e75a85dd70cb3d2f857e
Teorico <63122667+Teorico@users.noreply.github.com>
2020-04-09 14:39:59 -0600

    1
    1

--- js/script2.js
+++ js/script2.js
@@ -10,33 +10,32 @@
 
 
       var app={
-                questions:[
-                          {q:'¿Cuándo se publicó el primer código de circulación en Costa Rica?', options:['1912','1920','1850','1976'],answer:2},
-
-                          {q:'¿Con que fin se publicaron las Ley de tránsito 5930 y la Ley de Administración vial 6324? ?',options:['Con el fin de evitar y enfrentar la problemática del volumen de accidentes en la década de los 70´s.','Disminuir el tráfico vial','Regular los problemas viales','Con el fin de evitar y enfrentar la problemática del volumen de accidentes en la década de los 50´s'],answer:1},
-
-                          {q:'¿Cuándo se publicó la Ley de tránsito 5930?',options:['12 de octubre de 1976',' 12 de octubre 1920','22 de abril de 1993','24 de mayo de 1979'],answer:1},
-
-                          {q:'¿Cuándo circuló el primer carro en Costa Rica?',options:['1912','1944','1920','1922'],answer:1},
-
-                          {q:' ¿En qué década se dio un crecimiento inesperado de vehículos que produjo demasiados accidentes?',options:[' En la década de los 70.',' En la década de los 80.',' En la década de los 60.','Ninguna de las anteriores'],answer:1},
-
-                          {q:' ¿Cuándo fue publicada la Ley de administración vial 6324 en Costa Rica?',options:['24 de mayo de 1993','24 de mayo de 1979','12 de octubre 1920','12 de octubre de 1976'],answer:2},
-
-                          {q:'¿En qué década se da un crecimiento explosivo de vehículos y una transformación de la ciudades y pueblos? ',options:['En la década de los 80´s','En la década de los 70','A inicios de los 90','A finales del 1920'],answer:1},
-
-                          {q:'¿Cuál ley se crea debido al aumento de carros en la década de los 80? ',options:['Ley de tránsito por vías públicas terrestres 7331','Ley de tránsito 5930','Código de circulación','Ley de administración vial 6324'],answer:1},
-
-                          {q:'¿Cuándo se publicó la Ley de tránsito por vías públicas terrestres 7331? ',options:['26 octubre 2012','24 de mayo de 1979','22 de abril de 1993','12 de octubre 1920'],answer:3},
-
-                          {q:'¿Cuál ley sustituyó la Ley de tránsito por vías públicas terrestres 7331? ',options:['No sustituyó ninguna ley anterior','La ley 7331 sustituyó el código de circulación',' La ley 7331 sustituyó la Ley de tránsito 6324',' La ley 7331 sustituyó la Ley de tránsito 5930'],answer:4},
-
-                          {q:'¿Cuál es la ley que nos rige actualmente? ',options:['La ley 7331','La ley de transito por vías publicas y seguridad vial 9078','Ley de administración vial 6324','Ley de tránsito 5930 '],answer:2},
-
-                          {q:'¿Cuándo se publicó la Ley de Tránsito por Vías Públicas y seguridad vial 9078? ',options:['22 de abril de 1993','24 de mayo de 1979','26 octubre 2012','12 de octubre de 1976'],answer:3},
-
-                        
-
+                questions:[{q:'Todo costarricense puede trasladarse o permanecer en cualquier punto de la república o fuera de ella”, por lo que esto se define como derecho circular que se entiende como libertad de circulación. Para poder hacer efectivo este derecho debemos acatar la ley de tránsito.', options:['Artículo 130','Artículo 139','Artículo 1','Artículo 22'],answer:4},
+                {q:'¿Cuántos  artículos tiene la ley de tránsito 9078?', options:['8','9','252','23'],answer:3},
+                {q:'¿Quién puede cambiar una disposición del señalamiento de tránsito como por ejemplo;circular contra vía , habilitar el paso con el semáforo en rojo acorde  al Artículo 93 inciso a?', options:['MOPT','Oficiales de tránsito','Ministerio de Salud ','COSEVI'],answer:2},
+                {q:'¿Cuántos  transitorios tiene la ley de tránsito 9078?', options:['8','10','252','23'],answer:4},
+                {q:'Las multas más altas son las de tipo A: ', options:['Falso','Verdadero',' ',' '],answer:2},
+                {q:'Las multas más bajas  son las de tipo E: ', options:['Falso ','Verdadero',' ',' '],answer:2},
+                {q:'Según la Ley de tránsito 9078 se denomina infractor a la persona que: ', options:['Inclumpla una o más normas de la ley','Impone la multa o dicta una resolución administrativa o Judicial ','',''],answer:1},
+                {q:'Según la Ley de tránsito 9078 se denomina Sancionador a la persona que: ', options:['Inclumpla una o más normas de la ley','Impone la multa o dicta una resolución administrativa o Judicial que impone una sanción relacionada con el tránsito','',''],answer:2},
+                {q:'¿Cuánto porcentaje puede variar el monto de las multas de anualmente?', options:['30 %','5 %','12 %','14 %'],answer:4},
+                {q:'¿Qué porcentaje de los montos que se recaudan en multas va dirigido al COSEVI?', options:['3 %','69 %','5 %','23 %'],answer:2},
+                {q:'¿Qué porcentaje de los montos que se recaudan en multas va dirigido a PANI?', options:['3 %','69 %','5 %','23 %'],answer:4},
+                {q:'¿Qué porcentaje de los montos que se recaudan en multas va dirigido a la Cruz Roja?', options:['3 %','69 %','5 %','23 %'],answer:3},
+                {q:'¿Qué porcentaje de los montos que se recaudan en multas va dirigido a la Ministerio de Justicia?', options:['3 %','69 %','5 %','23 %'],answer:1},
+                {q:'Para conductores novatos o profesionales ¿Cuáles son niveles de alcohol en sangre y aire aspirado respectivamente, para que considere un  delito la conducción bajo efectos del alcohol? ', options:['Más de 0,50g de alcohol por litro de sangre o 0,25mg en aire aspirado.','Más de 0,75g de alcohol en sangre o 38mg en aire aspirado','Más de 0,50mg de alcohol por litro de sangre o 0,25g en aire aspirado.','Más de 0,75mg de alcohol en sangre o 38g en aire aspirado'],answer:1},
+                {q:'Para conductores comúnes ¿Cuáles son niveles de alcohol en sangre y aire aspirado respectivamente para que considere un delito la conducción bajo efectos del alcohol? ', options:['Más de 0,50g de alcohol por litro de sangre o 0,25mg en aire aspirado.','Más de 0,75g de alcohol en sangre o 38mg en aire aspirado','Más de 0,50mg de alcohol por litro de sangre o 0,25g en aire aspirado.','Más de 0,75mg de alcohol en sangre o 38g en aire aspirado'],answer:2},
+                {q:'¿Es considerado delito conducir bajo efectos de drogas "tóxicas", estupefacientes, o psicotrópicas?', options:['Falso ','Verdadero','',''],answer:2},
+                {q:'¿Es considerado delito conducir a partir de 150km/h?', options:['Falso ','Verdadero','',''],answer:2},
+                {q:'¿Es considerado delito participar de "piques"o competencia de velocidad ilegales?', options:['Verdadero','Falso','',''],answer:1},
+                {q:'¿Cada cuánto debe renovar la licencia un conductor que haya acumulado de 0 a 4 puntos y cúal es el costo del trámite respectivamente?', options:['4 años y el costo es de 100 % del precio trámite','3  años y el costo es de 100 % del precio trámite','6 años y el costo es de 50 % del precio trámite','6 años y el costo es de 100 % del precio trámite'],answer:3},
+                {q:'¿Cada cuánto debe renovar la licencia un conductor que haya acumulado de 5 a 8 puntos y cúal es el costo del trámite repectivamente?', options:['4 años y el costo es de 100 % del precio trámite','3  años y el costo es de 100 % del precio trámite','7 años y el costo es de 50 % del precio trámite','7 años y el costo es de 100 % del precio trámite'],answer:1},
+                {q:'¿Cada cuánto debe renovar la licencia un conductor que haya acumulado de 9 a 11 puntos y cúal es el costo del trámite respectivamente?', options:['4 años y el costo es de 100 % del precio trámite','3  años y el costo es de 100 % del precio trámite','8 años y el costo es de 50 % del precio trámite','8 años y el costo es de 100 % del precio trámite'],answer:2},
+                {q:'¿Cuál es el periodo de suspención para el conductor novato que acumule 6 o más puntos en la licencia?', options:['1 año ','6 meses ','2 meses ','2 años'],answer:1},
+                {q:'¿Cuál es el periodo de suspención para el conductor común o profesional que que acumule 12 o más puntos en la licencia?', options:['1 año ','6 meses ','2 meses ','3 años'],answer:1},
+                {q:'¿Qué sucede si los conductores ya sean profesionales, comunes o novatos acumulan 5 puntos o más en la licencia?', options:['No puede circular por el periodo de un año','Se suspende la licencia por un periodo de 1 mes ','Deben realizar un curso de sensibilización y reeducación vial como requisito obligatorio para renovar la licencia de conducir','Debe pagar el 100  % del valor del trámite. '],answer:3},
+                
+                  
                           ],
                 index:0,
                 load:function(){
